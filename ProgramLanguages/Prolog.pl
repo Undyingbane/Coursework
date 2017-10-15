c73690cbb1cf6e217dc75c8fbacad263a4c15791
Undyingbane <Richardjohnrobinson@gmail.com>
2017-10-15 15:51:15 -0700

    Delete Prolog.pl

--- ProgramLanguages/Prolog.pl
+++
@@ -1,9 +0,0 @@
-%Definitions
-m([eclipse, atom, euler, waring, jack, jimmy, chad]).
-f([java, ruby, curie, sql, jill]).
-family([eclipse, java, [ruby]).
-family([euler, curie, [jill, jack]]).
-family([waring, sql, jimmy, chad]).
-%Rules
-male(X) :- m(M), member(X,M).
-female(X) :- f(F), member(X,F).
