6a951b649a1f5d5ad88e3f04eb115c9600d4e2c2
Undyingbane <Richardjohnrobinson@gmail.com>
2017-10-16 14:57:37 -0700

    Update problem3.pl

--- ProgramLanguages/HW3/problem3.pl
+++ ProgramLanguages/HW3/problem3.pl
@@ -21,21 +21,3 @@ checkSet(X, Y, [X2 | RestX], [Y2 | RestY]) :-
     Y2 - Y \== X2 - X,
     Y2 - Y \== X - X2,
     checkSet( X,Y, RestX, RestY).
-
-
-%testUnique([X]).
-%testUnique( [X|Y] ) :- \+member(X,Y), testUnique(Y).
-
-
-
-%safeZone(_, []).
-%safeZone( [ X ], [ Y ] ) :-       X \== X2,
-%                                  Y \== Y2,
-%                                  Y2 - Y \== X2 - X,
-%                                  Y2- Y \== X - X2,
-%                                  safeZone(X/Y, Rest).
-
-%member(Item, [Item | T]).
-%member(Item, [H | T]) :- member(Item, T).
-
-%template([1/Y, 2/Y2, 3/Y3, 4/Y4, 5/Y5, 6/Y6, 7/Y7, 8/Y8]).
