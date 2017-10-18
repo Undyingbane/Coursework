61b4a3b21c08db18afe613b88c0d264c6751cc91
Undyingbane <Richardjohnrobinson@gmail.com>
2017-10-18 07:23:19 -0700

    MidtermHW

---
+++ ComputerPerformanceEvaluation/HW3/Problem2.cpp
@@ -0,0 +1,20 @@
+
+#include <math.h>
+static const int CYLINDERS = 900;
+static const int CAPACITY = 300;
+static const int RPM = 7200;
+static const int N = 100000;
+
+double t(int x) {return pow(x, .5);}
+
+static void solve5()
+{
+	static int headStartLocation;
+	static double rotationDelay;
+	for (int i = 0; i < N; i++)
+	{
+		
+
+	}
+
+}
